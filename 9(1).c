099fee4b863fa0b64130e70908ce5a771b47135c
Shooter636 <116818499+Shooter636@users.noreply.github.com>
2022-11-22 22:38:45 +0500

    main.c

--- 9(1).c
+++ 9(1).c
@@ -1,17 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-int F(int N)
+#include <string.h>
+int main(int argc, char* argv[])
 {
-	if (N <= 1) return 0;
-	else if (N == 2) return 1;
-	else return F(N - 1) + F(N - 2);
-}
-int main()
-{
-	printf_s("Enter a number:\n");
-	int n;
-	scanf_s("%d", &n);
-	int tmp = F(n);
-	printf_s("%d", tmp);
+	char* A = argv[1];
+	char* B = (char*)calloc(sizeof(char), strlen(A));
+	while (1)
+	{
+		gets_s(B, sizeof(B));
+		if (strlen(B) == 0) break;
+		int ascii[256] = { 0 };// в аски 255 символов
+		
+		for (int i = 0; i < strlen(A); i++)
+		{
+			ascii[A[i]]++;
+		}
+		int flag = 1;
+		for (int i = 0; i < strlen(B); i++)
+		{
+			if (ascii[B[i]] == 0)
+			{
+				printf_s("FALSE\n");
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1) printf_s("TRUE\n");
+	}
+	free(B);
 	return 0;
 }
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char* argv[])
+{
+	unsigned x = 0;
+	for (int i = 0; i < argc; i++)
+	{
+		unsigned tmp = atoi(argv[i]);
+		x |= (1 << tmp);
+	}
+	unsigned Day;
+	while (1)
+	{
+		printf_s("Enter a day number:\n");
+		scanf_s("%u", &Day);
+		if (Day > 31)
+		{
+			printf_s("Error\n");
+			continue;
+		}
+		if (Day == 0) break;
+		if ((x & (1 << Day)) == 0) printf_s("Great morning to coffee!\n");
+		else printf_s("Time to sleep!\n");
+	}
+}
+//https://volstr.ru/?p=36
