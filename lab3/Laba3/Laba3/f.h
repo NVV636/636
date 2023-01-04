2b02af71167d0907701904758fcafb14b0213f1a
Shooter636 <116818499+Shooter636@users.noreply.github.com>
2023-01-04 13:41:34 +0500

    Delete lab3 directory

--- lab3/Laba3/Laba3/f.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdlib.h>
-#include <stdio.h>
-#include "f.c"
-int Comp(char* str1, const char* str2);
-char* StrSubstr(char* str1, char* str2);
-int StrLen(char* str);
-void PrintString(char* str);
