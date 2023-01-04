57c49daab7e8cad7521227004399281e250b6f2c
Shooter636 <116818499+Shooter636@users.noreply.github.com>
2023-01-04 13:41:22 +0500

    Delete Project3 directory

--- Project3/Project3/func.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int SearchSubStringInString(char* str1, char* str2);
-int StrLen(char* str);
-int CountSubstring(char* str1, char* str2);
-char* SubstringReplacement(char* str1, char* str2, char* str3);
